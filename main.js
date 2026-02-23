ff0c1d08b38a1cd2a84498b39b14c8250e8d005d
Slatewarrior15 <lilmarcjunior@gmail.com>
2026-02-23 11:36:22 -0500

    Implement game logic

--- main.js
+++ main.js
@@ -1,24 +1,72 @@
+
+
 // Randomly return rock , paper , or scissors from the computer
 function getComputerChoice(){
     choice = Math.floor((Math.random() * 3) + 1)
     console.log(choice)
     switch(choice){
         case 1:
-            console.log("Rock");
-            break;
+            return("ROCK");
+            
         case 2:
-            console.log("Paper");
-            break;
+            return("PAPER");
+           
         case 3:
-            console.log("Scissors");
-            break;
+            return("SCISSORS");
+            
     }
 }
 
 // Get user's input
 function getHumanChoice(){
-    let choice = prompt("Rock, Paper, or Scissors")
-    console.log(choice)
+    let choice = prompt("ROCK, PAPER, or SCISSORS")
+    return(choice.toUpperCase())
 }
 
-getHumanChoice();
+
+function playGame(){
+    let playRound = function(humanChoice,computerChoice){
+        let humanScore = 0
+        let computerScore = 0
+
+        const humanWins = 
+        (humanChoice == "ROCK" & computerChoice == "SCISSORS")||
+        (humanChoice == "PAPER" & computerChoice == "ROCK")|| 
+        (humanChoice == "SCISSORS" & computerChoice == "PAPER") 
+
+        const tie = (humanChoice === computerChoice)
+
+        if (humanWins){
+            humanScore++
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+        
+        }
+        else if (tie){
+            console.log(`It is a tie! ${humanChoice} ties with ${computerChoice}`)
+        }
+
+        else{
+            computerScore++
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        }
+    }
+
+    for ( let rounds = 0; rounds < 5; rounds++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+
+    }
+
+    if (humanScore > computerScore){
+        console.log("You win!")
+    }
+    else{
+        console.log("You lose!, better luck next time")
+    }
+}   
+
+
+playGame();
+
+
